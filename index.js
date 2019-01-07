742b708638d6407160bb8a433124313a327106aa
Jackie <jacgut19@gmail.com>
2019-01-07 15:09:07 +0000

    get, post, put, delete ninja from/to the database

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-//const app = require('express');
-
 const express = require('express');
+
+const routes = require('./routes/api');
 
 // set up express app
 const app = express();
 
-
-app.get('/api', function(req, res){
-   console.log('GET request');
-   res.send({name: 'Yoshi'});
-});
+app.use('/api', require('./routes/api'));
 
 // listen for requests
 app.listen(process.env.port || 4000, function(){
     console.log('now listening for request');
-})
+});
---
+++ routes/api.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+// get a list of ninjas get from the database
+router.get('./ninjas', function(req, res){
+    res.send({type: 'GET'});
+});
+// add a new ninja to the db
+router.post('./ninjas', function(req, res){
+    res.send({type: 'POST'});
+});
+// update a ninja to the database
+router.put('./ninjas/:id', function(req, res){
+    res.send({type: 'PUT'});
+});
+// delete a ninja from the database
+router.get('./ninjas/:id', function(req, res){
+    res.send({type: 'DELETE'});
+});
+
+module.exports = router;
