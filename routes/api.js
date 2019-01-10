ee0c220e47ae6c30a2d7939ee2ba3fd0c6f5409c
Jackie <jacgut19@gmail.com>
2019-01-10 16:14:10 +0000

    Handling DELETE Requests

--- routes/api.js
+++ routes/api.js
@@ -21,7 +21,11 @@ router.put('./ninjas/:id', function(req, res, next){
 
 // delete a ninja from the database
 router.get('./ninjas/:id', function(req, res, next){
-    res.send({type: 'DELETE'});
+    //console.log(req.params.id);
+    Ninja.findByIdAndRemove({_id: req.params.id}).then(function(ninja){
+        res.send(ninja);
+    });
+   // res.send({type: 'DELETE'});
 });
 
 module.exports = router;
